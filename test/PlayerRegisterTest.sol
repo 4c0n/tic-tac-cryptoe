9acb03e870e20879fe1651e0bf72b211f38b3e71
Youri Westerman <y.westerman@scuti.asia>
2018-07-19 13:45:42 +0700

    Fixed test

--- test/PlayerRegisterTest.sol
+++ test/PlayerRegisterTest.sol
@@ -5,9 +5,10 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/PlayerRegister.sol";
 
 contract PlayerRegisterTest {
-  function registersAgain(string signature) internal returns (bool) {
+  function execute(string signature) internal returns (bool) {
     bytes4 sig = bytes4(keccak256(signature));
     address self = address(this);
+    return self.call(sig);
   }
 
   function testCanRegisterPlayer() public {
@@ -18,7 +19,7 @@ contract PlayerRegisterTest {
   function testCannotRegisterTheSamePlayerTwice() public {
     PlayerRegister register = new PlayerRegister();
     register.newPlayer("player0");
-    
+    Assert.isFalse(execute('newPlayer("player1")'), "Error was not produced!");
   }
 }
 
