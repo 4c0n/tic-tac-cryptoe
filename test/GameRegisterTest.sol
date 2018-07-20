ad7be506da6303765a3fb54008e21694f29900bc
Youri Westerman <tetracon@gmail.com>
2018-07-20 20:08:30 +0700

    Added stubs for the test cases for GameRegister

---
+++ test/GameRegisterTest.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.24;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/GameRegister.sol";
+
+contract GameRegisterTest {
+  function testCanStartNewGame() public {
+  }
+
+  function testCannotStartNewGameWhenNotRegistered() public {
+  }
+
+  function testCannotStartNewGameWhenAlreadyPlaying() public {
+  }
+
+  function testCanJoinGameThatIsWaitingForAnOpponent() public {
+  }
+}
+
