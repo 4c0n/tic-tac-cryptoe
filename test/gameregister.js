aa71094717d2566e26083b48cc7c3ba9680bf3a2
Youri Westerman <tetracon@gmail.com>
2018-07-21 17:24:38 +0700

    Implemented remaining test cases

--- test/gameregister.js
+++ test/gameregister.js
@@ -30,11 +30,32 @@ contract("GameRegister", function(accounts) {
   });
 
   it("should return the correct game count", function() {
+    return GameRegister.deployed().then(function(instance) {
+      return instance.getGameCount({from: accounts[0]});
+    }).then(function(count) {
+      assert.equal(count, 1);
+    });
   });
 
   it("should produce an error when the player is not registered", function() {
+    return GameRegister.deployed().then(function(instance) {
+      return instance.newGame({from: accounts[2]});
+    }).then(function() {
+      // This case should not happen
+      assert.equal(1, 0);
+    }).catch(function(e) {
+      assert.equal(e.message, "VM Exception while processing transaction: revert");
+    });
   });
 
   it("should produce an error when the player is already playing a game", function() {
+    return GameRegister.deployed().then(function(instance) {
+      return instance.newGame({from: accounts[0]});
+    }).then(function() {
+      // This case should not happen
+      assert.equal(0, 1);
+    }).catch(function(e) {
+      assert.equal(e.message, "VM Exception while processing transaction: revert");
+    });
   });
 });
