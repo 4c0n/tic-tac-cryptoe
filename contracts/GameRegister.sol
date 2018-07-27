8a220ff52a308eade3ed9b152c15681d0ea47b03
Youri Westerman <y.westerman@scuti.asia>
2018-07-27 15:28:02 +0700

    Using 2 spaces for indentation instead of 4

--- contracts/GameRegister.sol
+++ contracts/GameRegister.sol
@@ -3,55 +3,55 @@ pragma solidity ^0.4.24;
 import "./PlayerRegister.sol";
 
 contract GameRegister is PlayerRegister {
-    struct Game {
-        uint moveCount;
-    }
+  struct Game {
+    uint moveCount;
+  }
     
-    Game[] public games;
+  Game[] public games;
 
-    // if true the last added game is waiting (games[games.length - 1])
-    bool waitingForOpponent;
+  // if true the last added game is waiting (games[games.length - 1])
+  bool waitingForOpponent;
     
-    // Game id is also off by +1 for the same reason
-    mapping (uint => uint) playerToGame;
-    mapping (uint => uint) gameToPlayerThatStarted;
+  // Game id is also off by +1 for the same reason
+  mapping (uint => uint) playerToGame;
+  mapping (uint => uint) gameToPlayerThatStarted;
     
-    event QueuedGame(uint indexed _from, uint indexed id);
-    event StartGame(uint indexed _from, uint indexed _to);
+  event QueuedGame(uint indexed _from, uint indexed id);
+  event StartGame(uint indexed _from, uint indexed _to);
     
-    function requirePlayerIsNotAlreadyPlaying() private view {
-        require(
-            playerToGame[getPlayerIndex()] == 0,
-            "Player is already playing a game!"
-        );
-    }
+  function requirePlayerIsNotAlreadyPlaying() private view {
+    require(
+      playerToGame[getPlayerIndex()] == 0,
+      "Player is already playing a game!"
+    );
+  }
 
-    function getGameIndexThatIsWaiting() internal view returns (uint) {
-        return games.length - 1;
-    }
+  function getGameIndexThatIsWaiting() internal view returns (uint) {
+    return games.length - 1;
+  }
 
-    function newGame() public {
-        requirePlayerExist();
-        requirePlayerIsNotAlreadyPlaying();
+  function newGame() public {
+    requirePlayerExist();
+    requirePlayerIsNotAlreadyPlaying();
 
-        uint playerId = getPlayerIndex();
-        uint gameId;
-        if (waitingForOpponent) {
-            gameId = getGameIndexThatIsWaiting();
-            playerToGame[playerId] = gameId;
-            waitingForOpponent = false;
-            uint opponentId = gameToPlayerThatStarted[gameId];
-            emit StartGame(playerId, opponentId);
-        } else {
-            gameId = games.push(Game(0));
-            playerToGame[playerId] = gameId;
-            gameToPlayerThatStarted[gameId - 1] = playerId;
-            waitingForOpponent = true;
-            emit QueuedGame(playerId, gameId);
-        }
+    uint playerId = getPlayerIndex();
+    uint gameId;
+    if (waitingForOpponent) {
+      gameId = getGameIndexThatIsWaiting();
+      playerToGame[playerId] = gameId;
+      waitingForOpponent = false;
+      uint opponentId = gameToPlayerThatStarted[gameId];
+      emit StartGame(playerId, opponentId);
+    } else {
+      gameId = games.push(Game(0));
+      playerToGame[playerId] = gameId;
+      gameToPlayerThatStarted[gameId - 1] = playerId;
+      waitingForOpponent = true;
+      emit QueuedGame(playerId, gameId);
     }
+  }
 
-    function getGameCount() public view returns (uint) {
-        return games.length;
-    }
+  function getGameCount() public view returns (uint) {
+    return games.length;
+  }
 }
--- contracts/PlayerRegister.sol
+++ contracts/PlayerRegister.sol
@@ -3,47 +3,47 @@ pragma solidity ^0.4.24;
 import "./Ownable.sol";
 
 contract PlayerRegister is Ownable {
-    struct Player {
-        uint16 winCount;
-        uint16 lossCount;
-        string name;
-    }
+  struct Player {
+    uint16 winCount;
+    uint16 lossCount;
+    string name;
+  }
 
-    Player[] public players;
+  Player[] public players;
 
-    // default value == 0 so that we can still check if a value is set
-    // the index stored in the uint is off by +1.
-    mapping (address => uint) ownerToPlayer;
+  // default value == 0 so that we can still check if a value is set
+  // the index stored in the uint is off by +1.
+  mapping (address => uint) ownerToPlayer;
 
-    event NewPlayer(address indexed _from, string name);
+  event NewPlayer(address indexed _from, string name);
 
-    function requirePlayerExist() internal view returns (bool) {
-        require(
-            ownerToPlayer[msg.sender] != 0,
-            "Account is not registered as a player!"
-        );
-    }
+  function requirePlayerExist() internal view returns (bool) {
+    require(
+      ownerToPlayer[msg.sender] != 0,
+      "Account is not registered as a player!"
+    );
+  }
 
-    function getPlayer() private view returns (Player) {
-        return players[getPlayerIndex()];
-    }
+  function getPlayer() private view returns (Player) {
+    return players[getPlayerIndex()];
+  }
 
-    function getPlayerIndex() internal view returns (uint) {
-        return ownerToPlayer[msg.sender] - 1;
-    }
+  function getPlayerIndex() internal view returns (uint) {
+    return ownerToPlayer[msg.sender] - 1;
+  }
 
-    function newPlayer(string name) public {
-        require(
-            ownerToPlayer[msg.sender] == 0,
-            "Account is already registered as player!"
-        );
-        uint id = players.push(Player(0, 0, name));
-        ownerToPlayer[msg.sender] = id;
-        emit NewPlayer(msg.sender, name);
-    }
+  function newPlayer(string name) public {
+    require(
+      ownerToPlayer[msg.sender] == 0,
+      "Account is already registered as player!"
+    );
+    uint id = players.push(Player(0, 0, name));
+    ownerToPlayer[msg.sender] = id;
+    emit NewPlayer(msg.sender, name);
+  }
 
-    function getPlayerName() public view returns (string) {
-        requirePlayerExist();
-        return getPlayer().name;
-    }
+  function getPlayerName() public view returns (string) {
+    requirePlayerExist();
+    return getPlayer().name;
+  }
 }
