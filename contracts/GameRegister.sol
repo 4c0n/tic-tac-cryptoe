affac71d5dbb223707b749feead06b16ed5e82ab
Youri Westerman <tetracon@gmail.com>
2018-08-05 15:53:31 +0700

    Added getOpponentAddress function to allow retrieving opponent information

--- contracts/GameRegister.sol
+++ contracts/GameRegister.sol
@@ -6,23 +6,34 @@ contract GameRegister is PlayerRegister {
   struct Game {
     uint moveCount;
   }
-    
+
   Game[] public games;
 
   // if true the last added game is waiting (games[games.length - 1])
   bool waitingForOpponent;
-    
+
   // Game id is also off by +1 for the same reason
   mapping (uint => uint) playerToGame;
   mapping (uint => address) gameToPlayerThatStarted;
-    
+  mapping (uint => address) gameToPlayerThatJoined;
+
   event QueuedGame(address indexed _from, uint id);
   event StartGame(address indexed _from, address indexed _to);
-    
+
   function requirePlayerIsNotAlreadyPlaying() private view {
     require(
       playerToGame[getPlayerIndex()] == 0,
       "Player is already playing a game!"
+    );
+  }
+
+  function requireIsPlaying() private view {
+    requirePlayerExists();
+    uint playerId = getPlayerIndex();
+    uint gameId = playerToGame[playerId];
+    require(
+      (gameId != 0) && !(waitingForOpponent && getGameIndexThatIsWaiting() == (gameId - 1)),
+      "Not playing!"
     );
   }
 
@@ -54,6 +65,7 @@ contract GameRegister is PlayerRegister {
     if (waitingForOpponent) {
       gameId = getGameIndexThatIsWaiting();
       playerToGame[playerId] = gameId + 1;
+      gameToPlayerThatJoined[gameId] = msg.sender;
       waitingForOpponent = false;
       address opponentAddress = gameToPlayerThatStarted[gameId];
       emit StartGame(msg.sender, opponentAddress);
@@ -68,5 +80,19 @@ contract GameRegister is PlayerRegister {
 
   function getGameCount() public view returns (uint) {
     return games.length;
+  }
+
+  function getOpponentAddress() public view returns (address) {
+    requireIsPlaying();
+    uint gameId = playerToGame[getPlayerIndex()];
+    gameId--;
+
+    address started = gameToPlayerThatStarted[gameId];
+    address joined = gameToPlayerThatJoined[gameId];
+
+    if (started == msg.sender) {
+      return joined;
+    }
+    return started;
   }
 }
