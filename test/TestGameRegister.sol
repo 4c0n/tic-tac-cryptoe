0dd6d66b52e7adfc22ce58fd3ebb0647e3408012
Youri Westerman <tetracon@gmail.com>
2018-08-05 16:33:24 +0700

    Implemented remaining test cases

--- test/TestGameRegister.sol
+++ test/TestGameRegister.sol
@@ -18,10 +18,15 @@ contract GameRegisterProxy {
     register.newPlayer("player0");
     register.getOpponentAddress();
   }
+
+  function getOpponentAddressWithoutRegistering() public {
+    GameRegister register = new GameRegister();
+    register.getOpponentAddress();
+  }
 }
 
 contract TestGameRegister {
-  function testCanReturnCorrectOpponentAddressWhenPlayingTheGame() public {
+  function testCanReturnCorrectOpponentAddressToPlayerThatStartedWhenPlayingTheGame() public {
     GameRegister register = new GameRegister();
     register.newPlayer("player0");
     register.newGame();
@@ -38,6 +43,26 @@ contract TestGameRegister {
 
     address oppAddress = register.getOpponentAddress();
 
+    Assert.equal(proxyAddress, oppAddress, "Could not get correct opponent address!");
+  }
+
+  function testCanReturnCorrectOpponentAddressToPlayerThatJoinedWhenPlayingTheGame() public {
+    GameRegister register = new GameRegister();
+    register.newPlayer("player0");
+
+    ThrowProxy proxy = new ThrowProxy(address(register));
+    address proxyAddress = address(proxy);
+    GameRegister(proxyAddress).newPlayer("player1");
+    bool r1 = proxy.execute.gas(100000)();
+    Assert.isTrue(r1, "r1 was supposed to be true");
+
+    GameRegister(proxyAddress).newGame();
+    bool r2 = proxy.execute.gas(100000)();
+    Assert.isTrue(r2, "r2 was supposed to be true");
+
+    register.newGame();
+
+    address oppAddress = register.getOpponentAddress();
     Assert.equal(proxyAddress, oppAddress, "Could not get correct opponent address!");
   }
 
@@ -58,6 +83,11 @@ contract TestGameRegister {
  }
 
   function testCannotReturnOpponentAddressWhenNotRegistered() public {
+    GameRegisterProxy registerProxy = new GameRegisterProxy();
+    ThrowProxy throwProxy = new ThrowProxy(address(registerProxy));
+
+    GameRegisterProxy(address(throwProxy)).getOpponentAddressWithoutRegistering();
+    Assert.isFalse(throwProxy.execute(), "Did not produce error!");
   }
 }
 
