02df142ee96f58dfe50db7d41327d2c6bc5fb203
Youri Westerman <y.westerman@scuti.asia>
2018-08-30 15:36:57 +0700

    Implemented test case to verify that an error is produced when isItMyTurn() is called and the game is queued

---
+++ test/TestGameRegisterPart4.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.24;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "./ThrowProxy.sol";
+import "../contracts/GameRegister.sol";
+
+contract GameRegisterProxy {
+  function getIsItMyTurnWithQueuedGame() public {
+    GameRegister register = new GameRegister();
+    register.newPlayer("player1");
+    register.newGame();
+    register.isItMyTurn();
+  }
+}
+
+contract TestGameRegisterPart4 {
+  function testCannotReturnWhosTurnItIsWhenRegisteredButGameIsQueued() public {
+    GameRegisterProxy register = new GameRegisterProxy();
+    ThrowProxy throwProxy = new ThrowProxy(address(register));
+
+    GameRegisterProxy(address(throwProxy)).getIsItMyTurnWithQueuedGame();
+    bool r = throwProxy.execute.gas(2000000)();
+
+    Assert.isFalse(r, "Error was not produced!");
+  }
+/*
+  function testCannotMakeMoveWhenNotRegistered() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+
+  function testCannotMakeMoveWhenRegisteredButNoGameWasStarted() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+
+  function testCannotMakeMoveWhenRegisteredButGameIsQueued() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+
+  function testCannotMakeMoveWhenItIsNotThePlayerHisTurn() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+
+  function testCannotMakeMoveWhenTheCellIsNotFree() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+
+  function testCannotMakeMoveWhenNoMoreMovesArePossible() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+
+  function testCanMakeMove() public {
+    Assert.fail("Test is not implemented yet!");
+  }
+*/
+}
+
