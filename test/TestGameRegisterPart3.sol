b2225799758bed0ba03b0f72487c20bca5f0ba38
Youri Westerman <y.westerman@scuti.asia>
2018-08-30 11:39:29 +0700

    Implemented positive test cases for the isItMyTurn function

--- test/TestGameRegisterPart3.sol
+++ test/TestGameRegisterPart3.sol
@@ -5,31 +5,47 @@ import "truffle/DeployedAddresses.sol";
 import "./ThrowProxy.sol";
 import "../contracts/GameRegister.sol";
 
+contract GameRegisterProxy {
+  function getIsItMyTurnWithoutMoves() public returns (bool) {
+    GameRegister register = GameRegister(DeployedAddresses.MovesRegister());
+    register.newPlayer("player1");
+    register.newGame();
+
+    return register.isItMyTurn();
+  }
+
+  function getIsItMyTurnWhenAMoveWasMade() public returns (bool) {
+    GameRegister register = GameRegister(DeployedAddresses.MovesRegister());
+
+    return register.isItMyTurn();
+  }
+}
+
 contract TestGameRegisterPart3 {
-  function testCanReturnWhosTurnItIsWhenNoMovesWereMadeToThePlayerThatStarted() public {
-    GameRegister register = new GameRegister();
-    ThrowProxy proxy = new ThrowProxy(address(register));
+  GameRegisterProxy proxy;
+
+  function testCanReturnWhosTurnItIsWhenNoMovesWereMadeToThePlayerThatJoined() public {
+    GameRegister register = GameRegister(DeployedAddresses.MovesRegister());
 
     register.newPlayer("player0");
     register.newGame();
 
-    GameRegister(address(proxy)).newPlayer("player1");
-    bool r1 = proxy.execute.gas(100000)();
-    Assert.isTrue(r1, "r1 was supposed to be true");
-    GameRegister(address(proxy)).newGame();
-    bool r2 = proxy.execute.gas(100000)();
-    Assert.isTrue(r2, "r2 was supposed to be true");
-
-    Assert.isTrue(register.isItMyTurn(), "It was not the player his turn, but it should have been");
-  }
-
-  function testCanReturnWhosTurnItIsWhenNoMovesWereMadeToThePlayerThatJoined() public {
+    proxy = new GameRegisterProxy();
+    bool r = proxy.getIsItMyTurnWithoutMoves();
+    Assert.isFalse(r, "isItMyTurn should have returned false");
   }
 
   function testCanReturnWhosTurnItIsWhenMovesWereMadeToThePlayerThatStarted() public {
+    GameRegister register = GameRegister(DeployedAddresses.MovesRegister());
+
+    register.makeMove(0, 0);
+
+    Assert.isFalse(register.isItMyTurn(), "It is the player his turn, but it should not be");
   }
 
   function testCanReturnWhosTurnItIsWhenMovesWereMadeToThePlayerThatJoined() public {
+    bool r = proxy.getIsItMyTurnWhenAMoveWasMade();
+    Assert.isTrue(r, "It is not the player's turn, but it should be");
   }
 }
 
