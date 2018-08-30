4ce158931f4eba930edb41f2432d8bda79a38583
Youri Westerman <y.westerman@scuti.asia>
2018-08-30 16:35:06 +0700

    Added test case to verify that an error is produced when calling makeMove() but the outcome has already been decided

--- test/TestGameRegisterPart5.sol
+++ test/TestGameRegisterPart5.sol
@@ -67,11 +67,42 @@ contract TestGameRegisterPart5 {
 
     Assert.isFalse(r, "Error was not produced!");
   }
-/*
-  function testCannotMakeMoveWhenNoMoreMovesArePossible() public {
-    Assert.fail("Test is not implemented yet!");
-  }
 
+  function testCannotMakeMoveWhenNoMoreMovesArePossible() public {
+    GameRegister register = new GameRegister();
+    ThrowProxy proxy = new ThrowProxy(address(register));
+
+    register.newPlayer("player0");
+    register.newGame();
+
+    GameRegister(address(proxy)).newPlayer("player1");
+    bool r1 = proxy.execute.gas(100000)();
+    Assert.isTrue(r1, "r1 was supposed to be true");
+
+    GameRegister(address(proxy)).newGame();
+    bool r2 = proxy.execute.gas(200000)();
+    Assert.isTrue(r2, "r2 was supposed to be true");
+
+    register.makeMove(0, 0);
+
+    GameRegister(address(proxy)).makeMove(1, 0);
+    bool r3 = proxy.execute.gas(100000)();
+    Assert.isTrue(r3, "r3 was supposed to be true");
+
+    register.makeMove(0, 1);
+
+    GameRegister(address(proxy)).makeMove(1, 1);
+    bool r4 = proxy.execute.gas(100000)();
+    Assert.isTrue(r4, "r4 was supposed to be true");
+
+    register.makeMove(0, 2);
+
+    GameRegister(address(proxy)).makeMove(1, 2);
+    bool r = proxy.execute.gas(100000)();
+
+    Assert.isFalse(r, "Error was not produced!");
+  }
+/*
   function testCanMakeMove() public {
     Assert.fail("Test is not implemented yet!");
   }
